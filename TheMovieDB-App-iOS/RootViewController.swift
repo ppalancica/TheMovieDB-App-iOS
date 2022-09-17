87ac07aed90a26b4cfb782fe7ad86f3926edb64c
System Administrator <root@Pavels-MacBook-Pro.local>
2022-09-17 21:54:12 +0300

    Removed SceneDelegate.swift, Main.storyboard files and related configs

--- TheMovieDB-App-iOS/RootViewController.swift
+++ TheMovieDB-App-iOS/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  TheMovieDB-App-iOS
 //
 //  Created by Pavel Palancica on 9/17/22.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
     }
-
-
 }
-
